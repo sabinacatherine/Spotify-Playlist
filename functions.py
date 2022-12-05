2398d6377b660892d8e19b3377e1d933b1649179
Sabina <sabgrossman14@gmail.com>
2022-12-04 21:49:23 -0500

    Files are now successfully unpacking, playlist returns for sample song

--- functions.py
+++ functions.py
@@ -8,9 +8,9 @@ import pandas as pd
 pt_concat = pd.read_csv("https://github.com/sabinacatherine/Spotify-Playlist/blob/main/data/song_names.csv?raw=true")
 
 def get_playlist(song_name):
-    ssf_vectors = np.load("//Users/sabinagrossman/Documents/GitHub/song_vectors.npy")
+    ssf_vectors = np.fromfile("//Users/sabinagrossman/Documents/GitHub/song_vectors").reshape(-1,1)
     X = ssf_vectors
     tree = BallTree(X, leaf_size=2)
     ind = tree.query(X[np.where(pt_concat['SName'] == song_name)], k = 20)
-    playlist = pt_concat.iloc[ind[:0]]
+    playlist = pt_concat['SName'].loc[ind[:0]][:20]
     return playlist
